66c89c8e9503f6f3b1adfb8ab1ffa4463f1492ce
sijey04 <faminianochristianjude@gmail.com>
2026-05-07 05:59:25 +0800

    railway host

--- backend/django_api/django_api/urls.py
+++ backend/django_api/django_api/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -17,6 +18,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/swagger/', permanent=True)),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
